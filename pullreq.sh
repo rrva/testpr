b5e08e2576f3565957cb34178c5dec8f80b3f4ae
Ragnar Rova <ragnar.rova@gmail.com>
2015-10-25 19:00:10 +0100

    Working pullreq

--- pullreq.sh
+++ pullreq.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
-if [ ! -z "$ghprbActualCommit" ]; then
-    changed=$(for i in $(git diff-tree --no-commit-id --name-only -r $ghprbActualCommit); do
-        dirname $i;
-    done | sort | uniq | grep "src" | sed -e 's,^src/,,g')
-    for i in $changed; do
-        make -C src/$changed clean
-        make -C src/$changed
+commit="$ghprbActualCommit"
+
+containsElement () {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
+}
+
+findNearestMakefileDir() {
+    local x
+    local start
+    x="$1"
+    start=$(pwd)
+    while [ "$x" != "$start" ] && [ "$x" != "." ] && [ "$x" != "/" ]; do
+        if [[ -e "$x/Makefile" ]]; then
+            printf "%s\0" "$x"
+            break
+        fi
+        x=$(dirname "$x");
+    done
+}
+
+if [ ! -z "$commit" ]; then
+    changed=( \
+        $(git diff-tree --no-commit-id --name-only -r "$commit" \
+        | egrep "^src" | tr '\n' '\0' | xargs -0 dirname | sort | uniq ) \
+    )
+    makefiles=( \
+        $(for x in "${changed[@]}"; do
+            findNearestMakefileDir "$x"
+        done | sort | uniq)
+    )
+    for m in "${makefiles[@]}"; do
+        make -C "$m" clean
+        make -C "$m" 
     done
 else
     echo "Not a pull request build"
-fi;
+fi
