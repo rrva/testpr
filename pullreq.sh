2134475498948216f597cb93aae5d0ceb2b4d672
Ragnar Rova <ragnar.rova@gmail.com>
2015-10-25 19:38:22 +0100

    Comments

--- pullreq.sh
+++ pullreq.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 commit="$ghprbActualCommit"
 
+# find Makefile for a directory.
+# search backwards upto the workdir root
 findNearestMakefileDir() {
     local x
     local start
@@ -16,17 +18,20 @@ findNearestMakefileDir() {
 }
 
 if [ ! -z "$commit" ]; then
+    # collect all directories where a change has been made
     changed=( \
         $(git diff-tree --no-commit-id --name-only -r "$commit" \
           | egrep "^src" | tr '\n' '\0' | xargs -I {} -0 dirname {} \
           | sort | uniq \
          ) \
     )
+    # find the nearest makefile for each directory
     makefiles=( \
         $(for x in "${changed[@]}"; do
             findNearestMakefileDir "$x"
         done | sort | uniq)
     )
+    # run make for all changed directories
     for m in "${makefiles[@]}"; do
         make -C "$m" clean
         make -C "$m"
