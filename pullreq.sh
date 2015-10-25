5a140cfaf99f2d2e57113209d58fcde0da9e40b6
Ragnar Rova <ragnar.rova@gmail.com>
2015-10-25 19:18:51 +0100

    dead code

--- pullreq.sh
+++ pullreq.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 commit="$ghprbActualCommit"
 
-containsElement () {
-    local e
-    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
-    return 1
-}
-
 findNearestMakefileDir() {
     local x
     local start
